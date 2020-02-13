d2de920ef8ef41b27422921f9f413fd48a7a2a42
Lars Lorentz Ludvigsen <59617571+larsll@users.noreply.github.com>
2020-02-13 23:59:09 +0100

    feature: support specifying S3 endpoint URL (#251)

--- src/sagemaker_containers/_trainer.py
+++ src/sagemaker_containers/_trainer.py
@@ -65,7 +65,10 @@ def train():
         env = sagemaker_containers.training_env()
 
         region = os.environ.get("AWS_REGION", os.environ.get(_params.REGION_NAME_ENV))
-        intermediate_sync = _intermediate_output.start_sync(env.sagemaker_s3_output(), region)
+        s3_endpoint_url = os.environ.get(_params.S3_ENDPOINT_URL, None)
+        intermediate_sync = _intermediate_output.start_sync(
+            env.sagemaker_s3_output(), region, endpoint_url=s3_endpoint_url
+        )
 
         if env.framework_module:
             framework_name, entry_point_name = env.framework_module.split(":")
